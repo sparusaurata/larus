7c57f1b44fff9325aae94d2d22893c29d457c33b
Rémy Cerda <17063513+sparusaurata@users.noreply.github.com>
2023-01-07 17:07:37 +0100

    Index of the demo

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ it is intended for comments -->
         Ordered lists are wrapped into
         <?php print_code('<ol>...</ol>'); ?> tags.
     </li><li>
-        List items inside an (un)ordered list (like these four items)
+        List items inside an (un)ordered list (like these five items)
         are wrapped into <?php print_code('<li>...</li>'); ?> tags.
     </li><li>
         Headings of level 1 (resp. 2, 3) are wrapped into
@@ -113,7 +113,12 @@ it is intended for comments -->
 <p lang="en">
     If you want to put a skip above a paragraph (to separate it from the 
     previous paragraph), add the following attribute:
-    <?php print_code('class="skipabove"'); ?>. 
+    <?php print_code('class="skipabove"'); ?>.
+    As an example, see the following...
+</p>
+
+<p lang="en" class="skipabove">
+    ... paragraph.
 </p>
 
 
@@ -143,6 +148,15 @@ it is intended for comments -->
     </li>
 </ul>
 
+<p lang="en">
+    If MathJax is enabled (this is the case by default),
+    you can also write mathematical formulæ by putting some TeX code inside
+    <!-- Don't worry if you don't understand the line below -->
+    <span class="code"><span>\(</span>...<span>\)</span></span>
+    delimiters.
+    Let's try: \( \int_0^{+\infty} e^{-x^2} \mathrm{d}x = \frac{\sqrt{\pi}}2 \).
+</p>
+
 
 <h2 lang="en">Multilingual writing</h2>
 
@@ -157,12 +171,18 @@ it is intended for comments -->
 <p lang="en">
     If your website is supposed to support several languages, each element 
     should be translated in each language. When a page is loaded in some
-    language, for instance in English, the elements will only be displayed if:
+    language, for instance in English (<?php print_code('en'); ?>),
+    the elements will only be displayed if:
 </p>
+
 <ul lang="en">
     <li>they have an attribute <?php print_code('lang="en"'); ?>, </li>
     <li>they have no <?php print_code('lang'); ?> attribute. </li>
 </ul>
+
+<p lang="en">
+    Again, see the source code of this page as an example!
+</p>
 
 <p lang="en">
     To choose the language(s) of your website, and to further customise
@@ -180,18 +200,119 @@ it is intended for comments -->
 <h2 lang="en">Single page vs. multiple pages</h2>
 
 
+<p lang="en">
+    Two main architectures are possible:
+</p>
+
+<ul lang="en">
+    <li>
+        a <b>single-page</b> website, which means that it only has one page
+        and that the links in the main menu (if there is a main menu)
+        point to different sections of this page;
+    </li><li>
+        a <b>multiple-pages</b> website, which means that it contains several
+        webpages and the links in the main menu points to these different pages.
+    </li>
+</ul>
+
+<p lang="en">
+    If you want to keep the website short, you should consider the first option.
+    Otherwise, you could prefer the second one and divide your website into 
+    several pages (usual pages are “Research”, “Teaching”, etc.).
+</p>
+
+<p lang="en">
+    For each of these pages, create a file <b>name.php</b>
+    (you should take this <b>index.php</b> as a model).
+    Leave all the pages in the home directory of the server if you want to 
+    avoid some bugs!
+    Notice that the home page should always be <b>index.php</b>.
+    For example, this demo website contains two pages, <b>index.php</b>
+    and <b>lists.php</b>. 
+</p>
 
 
-<h2>
+<h2 id="heading">
     <span lang="en">The main menu</span>
 </h2>
 
+<p lang="en">
+    Let us have a look to the configuration of the main menu
+    (that will only be displayed if the option 
+    <?php print_code('$op_nav'); ?>
+    is set to <?php print_code('true'); ?>
+    in <b>config.php</b>).
+    This is the menu you see at the top of the page, under the title of the 
+    website.
+</p>
 
+<p lang="en">
+    The links of the menu are contained in the option 
+    <?php print_code('$op_nav_content'); ?> in <b>config.php</b>.
+    Let us describe the default value of this option, that generates the menu 
+    of this demo.
+</p>
+
+<ul lang="en">
+    <li>
+        The first two links point to <?php print_code('index.php'); ?>
+        and <?php print_code('lists.php'); ?>.
+        These are <b>inner links</b>, they point to other pages of the website.
+        <br>
+        The URL should be the (relative) path to the webpages,
+        but since all the webpages are stored in the same folder,
+        the path is just the name of the file.
+    </li><li>
+        The third link points to
+        <?php print_code('https://github.com/sparusaurata/larus'); ?>.
+        This is an <b>outer link</b>, it points to somewhere outside the
+        website.
+        <br>
+        Even if this is not very recommended in the main menu,
+        this can be achieved by providing the whole URL of the destination.
+        Notice that the corresponding item in
+        <?php print_code('$op_nav_content'); ?>
+        contains a parameter <?php print_code("'out' => true"); ?>:
+        this makes the link open in a new tab.
+    </li><li>
+        The last link points to <?php print_code('#heading'); ?>.
+        This is an <b>anchor link</b>, it points to somewhere <i>inside</i>
+        the page you are reading
+        (in this example, it points to the title “The main menu” above).
+        <br>
+        To achieve this, you must first place an <i>anchor</i> somewhere in 
+        the webpage, so that you can link to it.
+        This is done by adding an attribute <?php print_code('id="anchor"'); ?>
+        to the HTML element you want to link to.
+        Then, the URL pointing to this anchor is just
+        <?php print_code('#anchor'); ?>.
+        <br>
+        Notice that <?php print_code('id'); ?>'s must be <i>unique</i>,
+        so you cannot write:
+        <?php print_code('<h1 id="anchor" lang="en">Heading</h1>', true); ?>
+        <?php print_code('<h1 id="anchor" lang="fr">Titre</h1>'); ?>
+        <br>
+        Instead, you should write:
+        <?php print_code('<h1 id="anchor">', true); ?>
+        <?php print_code('<span lang="en">Heading</span>'); ?><br>
+        <?php print_code('<span lang="fr">Titre</span>'); ?><br>
+        <?php print_code('</h1>'); ?><br>
+        as it is done in the example given in this page!
+    </li>
+</ul>
+
+<p lang="en">
+    Basically you should use anchor links for the menu of a single-page website,
+    and inner links for the menu of a multiple-pages website.
+</p>
 
 
 <h2 lang="en">More options</h2>
 
-
+<p lang="en">
+    Additional options to customise the website are available in the file 
+    <b>config.php</b>.
+</p>
 
 
 <?php
